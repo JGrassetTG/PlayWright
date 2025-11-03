9c27b1c055bdc666a0399d28058733478b706f95
JGrassetTG <jean.grasset@testgroup.cl>
2025-11-03 07:27:17 -0500

    Update playwright.config.ts

--- playwright-carrito-main/carrito-playwright/playwright.config.ts
+++ playwright-carrito-main/carrito-playwright/playwright.config.ts
@@ -1,24 +1,30 @@
 import { defineConfig } from '@playwright/test';
 import dotenv from 'dotenv';
 
-dotenv.config(); // Cargar variables desde .env
+dotenv.config(); // Carga las variables del archivo .env
 
-const env = process.env.ENV || 'dev';
+const env = process.env.ENV || 'qa';
 
 const baseURLs: Record<string, string> = {
-  dev: 'https://saucedemo.com',
+  dev: 'https://www.saucedemo.com',
   qa: 'https://www.saucedemo.com',
   prod: 'https://www.saucedemo.com',
 };
 
 export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  retries: process.env.CI ? 1 : 0,
+  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
+
   use: {
     baseURL: baseURLs[env],
-    headless: false,
-    trace: 'on',
+    headless: true,
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
   },
-  testDir: './tests',
-  reporter: [['html', { outputFolder: 'playwright-report', open: 'always' }]],
+
   projects: [
     {
       name: `tests-${env}`,
